8a23e7a25c3827fa1a399a02a1c428f603b7b2fb
Arbuzzzi <safonov.mir@gmail.com>
2019-11-09 13:59:47 +0300

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,36 +10,36 @@ module.exports = {
   'rules': {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  //   'vue/max-attributes-per-line': [
-  //     'error',
-  //     {
-  //       singleline: 1,
-  //       multiline: {
-  //         max: 1,
-  //         allowFirstLine: false
-  //       }
-  //     }
-  //   ],
-  //   'quotes': ['error', 'single'],
-  //   // 'no-mixed-spaces-and-tabs': 0,
-  //   'comma-dangle': ['error', {
-  //     'arrays': 'only-multiline',
-  //     'objects': 'only-multiline',
-  //     'imports': 'never',
-  //     'exports': 'never',
-  //     'functions': 'never'
-  //   }],
-  //   'vue/html-self-closing': ['error', {
-  //     'html': {
-  //       'void': 'any',
-  //       'normal': 'any',
-  //       'component': 'always'
-  //     },
-  //     'svg': 'always',
-  //     'math': 'always'
-  //   }],
-  // },
-  // parserOptions: {
-  //   parser: 'babel-eslint'
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'quotes': ['error', 'single'],
+    // 'no-mixed-spaces-and-tabs': 0,
+    'comma-dangle': ['error', {
+      'arrays': 'only-multiline',
+      'objects': 'only-multiline',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never'
+    }],
+    'vue/html-self-closing': ['error', {
+      'html': {
+        'void': 'any',
+        'normal': 'any',
+        'component': 'always'
+      },
+      'svg': 'always',
+      'math': 'always'
+    }],
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   }
 }
