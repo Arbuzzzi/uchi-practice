fce5b2d5fdea19349350a6355d49d516e8ba5a25
Arbuzzzi <safonov.mir@gmail.com>
2019-11-09 14:09:22 +0300

    updates

---
+++ dist/precache-manifest.a56b0e09e8dda8af21cb98d901057bdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7901c65e9140a172b0ac",
+    "url": "css/app.8afec6da.css"
+  },
+  {
+    "revision": "f96ad47ac295c993f025",
+    "url": "css/chunk-vendors.4eaf6de0.css"
+  },
+  {
+    "revision": "c06ae83eee1a63f99d6fa265d4cd29d8",
+    "url": "index.html"
+  },
+  {
+    "revision": "7901c65e9140a172b0ac",
+    "url": "js/app.9a84526f.js"
+  },
+  {
+    "revision": "f96ad47ac295c993f025",
+    "url": "js/chunk-vendors.e8ea3a45.js"
+  },
+  {
+    "revision": "862ae912292783256b43672e7567f732",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
